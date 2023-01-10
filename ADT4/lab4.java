25f844b5c9be4b7e3de265ebf6d01b79d705e01d
penplub <71209221+penplub@users.noreply.github.com>
2023-01-10 23:14:22 +0700

    Add files via upload

---
+++ ADT4/lab4.java
@@ -0,0 +1,29 @@
+/**
+ * lab4
+ */
+public class lab4 {
+
+    public static void main(String[] args) {
+        int size = 10;
+        String arr[][] = new String[size][size]; 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = "0";
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(i == j){
+                    System.out.printf("%3s", count);
+                }else{
+                    System.out.printf("%3s", arr[i][j]);
+                }
+            }
+            count++;
+            System.out.println();
+
+    }
+}
+}
